e1f9876207ea0727f7f76a42ed05fc399e398f3b
Tzuzul Code <mail@tzuzulcode.com>
2022-02-03 14:59:40 -0600

    Pasando Safe-args

--- FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/DetailsFragment.kt
+++ FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/DetailsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.findNavController
 
 class DetailsFragment : Fragment() {
 
@@ -14,6 +17,23 @@ class DetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_details, container, false)
+        val goHomeButton = rootView.findViewById<Button>(R.id.go_home_button)
+        val detailsText = rootView.findViewById<TextView>(R.id.details)
+
+        val bundle = DetailsFragmentArgs.fromBundle(requireArguments())
+
+        val name = bundle.name
+        val profession = bundle.profession
+        val age = bundle.age
+
+        detailsText.text = "Registro exitoso! ${name} (${age}). Profesión: ${profession}"
+
+        goHomeButton.setOnClickListener {
+            // Haciendo navegacion
+            rootView.findNavController().navigate(R.id.action_detailsFragment_to_welcomeFragment)
+        }
+
+        return rootView
     }
 }
--- FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/FormFragment.kt
+++ FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/FormFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
 
 class FormFragment : Fragment() {
 
@@ -14,6 +17,19 @@ class FormFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_form, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_form, container, false)
+        val goDetailsButton = rootView.findViewById<Button>(R.id.go_details_button)
+
+
+        goDetailsButton.setOnClickListener {
+            // Haciendo navegacion
+            val name = rootView.findViewById<EditText>(R.id.name).text.toString()
+            val profession = rootView.findViewById<EditText>(R.id.profession).text.toString()
+            val age = rootView.findViewById<EditText>(R.id.age).text.toString().toInt()
+            val action = FormFragmentDirections.actionFormFragmentToDetailsFragment(name,profession,age)
+            rootView.findNavController().navigate(action)
+        }
+
+        return rootView
     }
 }
