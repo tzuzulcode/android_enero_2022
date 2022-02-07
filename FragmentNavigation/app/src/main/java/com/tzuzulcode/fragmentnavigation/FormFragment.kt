6d68f2e1b89b8f912bb6a303a1b4ec9cb7fcae13
Tzuzul Code <mail@tzuzulcode.com>
2022-02-07 14:04:40 -0600

    Fragment navigation y themes

--- FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/FormFragment.kt
+++ FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/FormFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.navigation.findNavController
 
 class FormFragment : Fragment() {
@@ -23,11 +24,19 @@ class FormFragment : Fragment() {
 
         goDetailsButton.setOnClickListener {
             // Haciendo navegacion
-            val name = rootView.findViewById<EditText>(R.id.name).text.toString()
-            val profession = rootView.findViewById<EditText>(R.id.profession).text.toString()
-            val age = rootView.findViewById<EditText>(R.id.age).text.toString().toInt()
-            val action = FormFragmentDirections.actionFormFragmentToDetailsFragment(name,profession,age)
-            rootView.findNavController().navigate(action)
+            val nameText = rootView.findViewById<EditText>(R.id.name).text
+            val professionText = rootView.findViewById<EditText>(R.id.profession).text
+            val ageText = rootView.findViewById<EditText>(R.id.age).text
+
+            if(nameText.isEmpty() || professionText.isEmpty() || ageText.isEmpty()){
+                Toast.makeText( activity,"Rellena los campos",Toast.LENGTH_SHORT).show()
+            }else{
+                val name = nameText.toString()
+                val profession = professionText.toString()
+                val age = ageText.toString().toInt()
+                val action = FormFragmentDirections.actionFormFragmentToDetailsFragment(name,profession,age)
+                rootView.findNavController().navigate(action)
+            }
         }
 
         return rootView
