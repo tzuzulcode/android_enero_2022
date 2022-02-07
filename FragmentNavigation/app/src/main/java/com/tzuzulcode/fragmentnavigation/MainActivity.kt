38221b2f2bba728a9cf6fcc87a706e85aea049cb
Tzuzul Code <mail@tzuzulcode.com>
2022-02-07 15:06:20 -0600

    Action bar config

--- FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/MainActivity.kt
+++ FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/MainActivity.kt
@@ -2,10 +2,31 @@ package com.tzuzulcode.fragmentnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Utilizando nuestro custom toolbar
+        menuInflater.inflate(R.menu.menu_toolbar,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navHostController = findNavController(R.id.nav_host_fragment)
+        //return item.onNavDestinationSelected(navController = navHostController)
+        return item.onNavDestinationSelected(navHostController) || super.onOptionsItemSelected(item)
+    }
+
 }
