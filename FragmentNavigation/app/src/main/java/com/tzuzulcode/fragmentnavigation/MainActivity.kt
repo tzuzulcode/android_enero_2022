e98f28abf1057e42f8895ac08eb90bedbb9d69ba
Tzuzul Code <mail@tzuzulcode.com>
2022-02-16 14:30:44 -0600

    Navegación por fragmentos

--- FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/MainActivity.kt
+++ FragmentNavigation/app/src/main/java/com/tzuzulcode/fragmentnavigation/MainActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +23,27 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.welcomeFragment,R.id.formFragment),drawer
+        )
 
-        val builder = AppBarConfiguration.Builder(navController.graph)
+        //Mejorar el menu del drawer. Mostrar siempre
+
+        /*val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawer)
         val appBarConfiguration = builder.build()
+         */
         toolbar.setupWithNavController(navController,appBarConfiguration)
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        bottomNavigationView.setupWithNavController(navController)
+        //val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        //bottomNavigationView.setupWithNavController(navController)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        NavigationUI.setupWithNavController(navView,navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
